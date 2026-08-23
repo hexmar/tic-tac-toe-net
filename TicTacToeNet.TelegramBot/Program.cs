4e7a6d08ad692aebcb228b074e2a5f081faf8044
Evgeny Vyazovtsev <evo118@outlook.com>
2026-08-23 17:47:11 +0300

    Add project

---
+++ TicTacToeNet.TelegramBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToeNet.TelegramBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
